e318df5a605803b6708252db860722b74d3ec96e
amitkrraj <amitkrraj.cse@gmail.com>
2021-09-24 09:29:52 +0530

    add links to projects

--- js/mailto.js
+++ js/mailto.js
@@ -5,7 +5,7 @@ const cmessage = document.getElementById('formmessage');
 const csend = document.getElementById('send-btn');
 
 csend.addEventListener("click",()=>{
-    const link = "mailto:rajabhishekmaurya3@gmail.com?subject=" + cname.value + "&body=" + cname.value + "%0A" + cemail.value + "%0A" + csubject.value + "%0A" + cmessage.value;
+    const link = "mailto:amitkrraj.cse@gmail.com?subject=" + cname.value + "&body=" + cname.value + "%0A" + cemail.value + "%0A" + csubject.value + "%0A" + cmessage.value;
     console.log(link);
     window.open(link);
 });
